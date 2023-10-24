0dd35c66962b8df07a3ee15f1d7a328cff9f1f31
mohammed-sherif34 <mohammedshereef342002@gmail.com>
2023-10-25 01:12:16 +0300

    fetch newest book methode

--- lib/Features/home/presentation/manager/featured_book_cubit/featured_book_cubit.dart
+++ lib/Features/home/presentation/manager/featured_book_cubit/featured_book_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/models/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_book_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.homeRepo) : super(FeaturedBookInitial());
+  HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBook() async {
+    emit(FeaturedBookLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+        (failure) => FeaturedBookFailure(errMessage: failure.errMessage),
+        (books) => emit(FeaturedBookSuccess(books: books)));
+  }
 }
---
+++ lib/Features/home/presentation/manager/newest_book_cubit/newest_book_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/models/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_book_state.dart';
+
+class NewestBookCubit extends Cubit<NewestBookState> {
+  NewestBookCubit(this.homeRepo) : super(NewestBookInitial());
+  HomeRepo homeRepo;
+  Future<void> fetchFeaturedBook() async {
+    emit(NewestBookLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) => NewestBookFailure(errMessage: failure.errMessage),
+        (books) => emit(NewestBookSuccess(books: books)));
+  }
+}
